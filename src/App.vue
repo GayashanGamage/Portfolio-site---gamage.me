a7653c9530a5ed1b7fa31662c166135b68c51f32
gayashan <gayashan.randimagamage@gmail.com>
2024-09-09 11:20:59 +0530

    re structure project

--- src/App.vue
+++ src/App.vue
@@ -1,60 +1,15 @@
 <template>
-  <div class="levelOne-container">
-    <div class="levelTwo-container menu">
-      <Menu></Menu>
-    </div>
-    <div class="levelTwo-container description">
-      <Introduction></Introduction>
-    </div>
-    <div class="levelTwo-container content">
-      <router-view></router-view>
-    </div>
-  </div>
+  <div class="level-one-container">main window</div>
 </template>
 
-<script setup>
-import Introduction from "./components/Introduction.vue";
-import Menu from "./components/Menu.vue";
-</script>
+<script setup></script>
 
 <style>
 * {
   margin: 0;
 }
 html {
-  font-family: "Poppins", sans-serif;
+  font-family: "", sans-serif;
   font-size: 14px;
-}
-body {
-  background-color: var(--background-color);
-}
-.levelOne-container {
-  width: 100%;
-  margin: 0 auto;
-  display: grid;
-  grid-template-columns: repeat(2, 1fr);
-  grid-template-areas:
-    "description menu"
-    "description content";
-}
-.levelTwo-container.menu {
-  grid-area: menu;
-  position: sticky;
-  top: 0;
-  height: 15vh;
-}
-.levelTwo-container.description {
-  grid-area: description;
-  position: sticky;
-  top: 0;
-  height: 100vh;
-  margin: 0;
-}
-.levelTwo-container.content {
-  grid-area: content;
-  min-height: 85vh;
-  max-height: auto;
-  margin: 0px 0 0 0;
-  background-color: var(--background-color-content);
 }
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,39 +1,8 @@
-import Blog from "@/components/Blog.vue";
-import IndividualWork from "@/components/IndividualWork.vue";
-import Leetcode from "@/components/Leetcode.vue";
-import Mywork from "@/components/Mywork.vue";
-import Techstack from "@/components/Techstack.vue";
 import { createRouter, createWebHistory } from "vue-router";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: "/",
-      name: "home",
-      component: Mywork,
-    },
-    {
-      path: "/project/:projectName",
-      name: "singleProject",
-      component: IndividualWork,
-    },
-    {
-      path: "/leetcode",
-      name: "leetcode",
-      component: Leetcode,
-    },
-    {
-      path: "/blog",
-      name: "blog",
-      component: Blog,
-    },
-    {
-      path: "/techstack",
-      name: "techstack",
-      component: Techstack,
-    },
-  ],
+  routes: [],
 });
 
 export default router;

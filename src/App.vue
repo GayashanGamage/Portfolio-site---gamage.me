b878b28921c62ee4fe33877e9097b5186543b25f
gayashan <gayashan.randimagamage@gmail.com>
2025-03-26 13:24:48 +0530

    populate pinia store date and bring in to app.vue. add project images

--- src/App.vue
+++ src/App.vue
@@ -24,16 +24,16 @@
           <!-- link section -->
            <div>
             <div>
-              <img :src="profileLinkIcons.linkedin" alt="linkedin icon" class="w-6 h-6">
-              <p><a :href="profileLinks.linkedin" target="_blank">LinkedIn</a></p>
+              <img :src="store.profileLinkIcons.linkedin" alt="linkedin icon" class="w-6 h-6">
+              <p><a :href="store.profileLinks.linkedin" target="_blank">LinkedIn</a></p>
             </div>
             <div>
-              <img :src="profileLinkIcons.github" alt="github icon" class="w-6 h-6">
-              <p><a :href="profileLinks.github" target="_blank">GitHub</a></p>
+              <img :src="store.profileLinkIcons.github" alt="github icon" class="w-6 h-6">
+              <p><a :href="store.profileLinks.github" target="_blank">GitHub</a></p>
             </div>
             <div>
-              <img :src="profileLinkIcons.medium" alt="github icon" class="w-6 h-6">
-              <p><a :href="profileLinks.medium" target="_blank">Medium</a></p>
+              <img :src="store.profileLinkIcons.medium" alt="github icon" class="w-6 h-6">
+              <p><a :href="store.profileLinks.medium" target="_blank">Medium</a></p>
             </div>
            </div>
          </div>
@@ -45,7 +45,7 @@
     <div class="flex flex-col w-1/2 h-50 border">
       <h3>TECH STACK</h3>
       <div class="flex">
-        <div v-for="item in techStack" class="border">
+        <div v-for="item in store.techStack" class="border">
           <img :src="item.icon" :alt="item.name" class="w-6 h-6">
           <p>{{ item.name }}</p>
         </div>
@@ -56,23 +56,24 @@
   <div class="flex justify-center py-10">
     <div class="flex flex-col w-1/2 h-auto border">
       <h3>PROJECTS</h3>
-      <div class="flex flex-col p-4 border">
-        <div class="bg-amber-700 w-full h-120 border"></div>
-        <h4>Project Title</h4>
-        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cumque accusantium temporibus, recusandae corporis id suscipit velit quis illum explicabo voluptate totam tenetur eos quas similique aspernatur molestias sapiente quo aut?</p>
+      <div class="flex flex-col p-4 border" v-for="item in store.projectData"> 
+        <!-- <div class="bg-amber-700 w-full h-120 border"></div> -->
+         <img :src="item.main_image" alt="item.name" class="w-full h-100">
+        <h4>{{ item.name }}</h4>
+        <p>{{ item.description }}</p>
         <!-- tech stack container -->
         <div class="flex flex-row">
-          <!-- tech item -->
-          <div class="border" v-for="item in techStack">
-            <img :src="item.icon" :alt="item.name" class="w-6 h-6">
-            <p>{{ item.name }}</p>
+          <!-- selected tech stack -->
+          <div class="border" v-for="i in item.techStack">
+            <img :src="i.icon" :alt="i.name" class="w-6 h-6">
+            <p>{{ i.name }}</p>
           </div>
         </div>
         <!-- project links -->
          <div class="flex flex-row">
-          <div class="flex flex-row">
-            <span class="material-icons">language</span>
-            <p>Live Project</p>
+          <div class="flex flex-row" v-for="i in item.links">
+            <span class="material-icons">{{ i.icon }}</span>
+            <p>{{ i.text }}</p>
           </div>
          </div>
       </div>
@@ -81,106 +82,9 @@
 </template>
 
 <script setup>
-
-const blog_url = 'https://medium.com/@gayashangamage'
-
-// link icons
-const profileLinkIcons = {
-  'medium' : '/public/medium.png',
-  'linkedin' : "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/linkedin/linkedin-plain.svg",
-  'github' : "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/github/github-original.svg"
-}
-
-// profile links
-const profileLinks = {
-  'medium' : 'https://medium.com/@gayashangamage',
-  'linkedin' : 'https://www.linkedin.com/in/gayashangamage/',
-  'github' : 'https://github.com/GayashanGamage'
-}
-
-// tech stack types
-const techStackTypes = [
-  'language',
-  'frontend',
-  'backend',
-  'database',
-  'devops',
-  'tools'
-]
-
-// tech stack
-const techStack = [
-  {
-    'type' : techStackTypes[0],
-    'name' : 'JavaScript',
-    'icon' : "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/javascript/javascript-original.svg"
-  },
-  {
-    'type' : techStackTypes[0],
-    'name' : 'Python',
-    'icon' : "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/python/python-original.svg"
-  },
-  {
-    'type' : techStackTypes[1],
-    'name' : 'HTML',
-    'icon' : "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/html5/html5-original.svg"
-  },
-  {
-    'type' : techStackTypes[1],
-    'name' : 'CSS',
-    'icon' : "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/css3/css3-original.svg"
-  },
-  {
-    'type' : techStackTypes[1],
-    'name' : 'Vue.js',
-    'icon' : 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/vuejs/vuejs-original.svg'
-  },
-  {
-    'type' : techStackTypes[1],
-    'name' : 'Tailwind CSS',
-    'icon' : "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/tailwindcss/tailwindcss-original.svg" 
-  },
-  {
-    'type' : techStackTypes[2],
-    'name' : 'FastAPI',
-    'icon' : "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/fastapi/fastapi-original.svg"
-  },
-  {
-    'type' : techStackTypes[2],
-    'name' : 'Flask',
-    'icon' : "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/flask/flask-original.svg"
-  },
-  {
-    'type' : techStackTypes[3],
-    'name' : 'MongoDB',
-    'icon' : "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/mongodb/mongodb-original.svg"
-  },
-  {
-    'type' : techStackTypes[3],
-    'name' : 'Supabase',
-    'icon' : "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/supabase/supabase-original.svg"
-  },
-  {
-    'type' : techStackTypes[4],
-    'name' : 'Docker',
-    'icon' : "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/docker/docker-plain.svg"
-  },
-  {
-    'type' : techStackTypes[4],
-    'name' : 'AWS',
-    'icon' : "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/amazonwebservices/amazonwebservices-original-wordmark.svg"
-  },
-  {
-    'type' : techStackTypes[5],
-    'name' : 'Postman',
-    'icon' : "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/postman/postman-original.svg"
-  },
-  {
-    'type' : techStackTypes[5],
-    'name' : 'GIT',
-    'icon' : "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/git/git-original.svg"
-  },
-]
+import { ref } from 'vue';
+import { portfolioStore } from './stores/counter';
+const store =  portfolioStore()
 
 </script>
 
--- src/stores/counter.js
+++ src/stores/counter.js
@@ -1,1 +1,192 @@
+import { ref, computed } from "vue";
 import { defineStore } from "pinia";
+
+export const portfolioStore = defineStore("Portfolio", () => {
+    const profileLinkIcons = {
+        'medium' : '/public/medium.png',
+        'linkedin' : "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/linkedin/linkedin-plain.svg",
+        'github' : "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/github/github-original.svg"
+        }
+    
+    const profileLinks = {
+        'medium' : 'https://medium.com/@gayashangamage',
+        'linkedin' : 'https://www.linkedin.com/in/gayashangamage/',
+        'github' : 'https://github.com/GayashanGamage'
+        }
+
+    const techStackTypes = [
+        'language',
+        'frontend',
+        'backend',
+        'database',
+        'devops',
+        'tools'
+        ]
+
+    const techStack = [
+        {
+            'id' : 0,
+            'type' : techStackTypes[0],
+            'name' : 'JavaScript',
+            'icon' : "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/javascript/javascript-original.svg"
+        },
+        {
+            'id' : 1,
+            'type' : techStackTypes[0],
+            'name' : 'Python',
+            'icon' : "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/python/python-original.svg"
+        },
+        {
+            'id' : 2,
+            'type' : techStackTypes[1],
+            'name' : 'HTML',
+            'icon' : "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/html5/html5-original.svg"
+        },
+        {
+            'id' : 3,
+            'type' : techStackTypes[1],
+            'name' : 'CSS',
+            'icon' : "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/css3/css3-original.svg"
+        },
+        {
+            'id' : 4,
+            'type' : techStackTypes[1],
+            'name' : 'Vue.js',
+            'icon' : 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/vuejs/vuejs-original.svg'
+        },
+        {
+            'id' : 5,
+            'type' : techStackTypes[1],
+            'name' : 'Tailwind CSS',
+            'icon' : "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/tailwindcss/tailwindcss-original.svg" 
+        },
+        {
+            'id' : 6,
+            'type' : techStackTypes[2],
+            'name' : 'FastAPI',
+            'icon' : "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/fastapi/fastapi-original.svg"
+        },
+        {
+            'id' : 7,
+            'type' : techStackTypes[2],
+            'name' : 'Flask',
+            'icon' : "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/flask/flask-original.svg"
+        },
+        {
+            'id' : 8,
+            'type' : techStackTypes[3],
+            'name' : 'MongoDB',
+            'icon' : "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/mongodb/mongodb-original.svg"
+        },
+        {
+            'id' : 9,
+            'type' : techStackTypes[3],
+            'name' : 'Supabase',
+            'icon' : "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/supabase/supabase-original.svg"
+        },
+        {
+            'id' : 10,
+            'type' : techStackTypes[4],
+            'name' : 'Docker',
+            'icon' : "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/docker/docker-plain.svg"
+        },
+        {
+            'id' : 11,
+            'type' : techStackTypes[4],
+            'name' : 'AWS',
+            'icon' : "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/amazonwebservices/amazonwebservices-original-wordmark.svg"
+        },
+        {
+            'id' : 12,
+            'type' : techStackTypes[5],
+            'name' : 'Postman',
+            'icon' : "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/postman/postman-original.svg"
+        },
+        {
+            'id' : 13,
+            'type' : techStackTypes[5],
+            'name' : 'GIT',
+            'icon' : "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/git/git-original.svg"
+        },
+        ]
+
+    const projectData = [
+        {
+            'name' : 'Curry Hut',
+            'description' : 'Delivery companies like Uber and Pickme charge 20% food price + Delivery charges. but curry hut is a platform that connects customers directly with the restaurant. So the restaurent owner can save 20% of the food price and custorms can enjoy Free Delivery charges. win win situation for both parties.',
+            'techStack' : [techStack[0], techStack[1], techStack[4], techStack[5], techStack[6], techStack[8], techStack[10]],
+            'main_image' : 'public/curryhut.webp',
+            'links' : [
+                {
+                    'icon' : 'language',
+                    'text' : 'Live project',
+                    'url' : 'https://curryhut.gamage.me/'
+                },
+                {
+                    'icon' : 'code',
+                    'text' : 'GitHub',
+                    'url' : 'https://github.com/GayashanGamage/curryhut'
+                },
+                {
+                    'icon' : 'play_circle',
+                    'text' : 'Youtuve Demo',
+                    'url' : 'https://www.youtube.com/watch?v=AaCnBOqyvIM'
+                },
+            ]
+        },
+        {
+            'name' : 'Sigiri Price',
+            'description' : 'Price fluctuation in e-commerce platforms is a common issue. Sigiri Price is a web application that helps to track the price changes of products in sinhagiri.lk e-commerce platforms.',
+            'techStack' : [techStack[4], techStack[5], techStack[6], techStack[8]],
+            'main_image' : 'public/sigiri price.png',
+            'links' : [
+                {
+                    'icon' : 'language',
+                    'text' : 'Live project',
+                    'url' : 'https://sigiriprice.gamage.me/'
+                },
+                {
+                    'icon' : 'code',
+                    'text' : 'GitHub',
+                    'url' : 'https://github.com/GayashanGamage/sinha.price-backend'
+                },
+                {
+                    'icon' : 'play_circle',
+                    'text' : 'Youtuve Demo',
+                    'url' : 'https://www.youtube.com/watch?v=AaCnBOqyvIM'
+                },
+            ]
+        },
+        {
+            'name' : 'FingureTune',
+            'description' : 'Stay on your confort zoon and try master keyborde with realtime analitics ',
+            'techStack' : [techStack[4], techStack[5], techStack[6]],
+            'main_image' : 'public/finguretune.png',
+            'links' : [
+                {
+                    'icon' : 'language',
+                    'text' : 'Live project',
+                    'url' : 'https://sigiriprice.gamage.me/'
+                },
+                {
+                    'icon' : 'code',
+                    'text' : 'GitHub',
+                    'url' : 'https://github.com/GayashanGamage/sinha.price-backend'
+                },
+                {
+                    'icon' : 'play_circle',
+                    'text' : 'Youtuve Demo',
+                    'url' : 'https://www.youtube.com/watch?v=AaCnBOqyvIM'
+                },
+            ]
+        },
+    ]
+
+    return{
+        profileLinkIcons,
+        profileLinks,
+        techStackTypes,
+        techStack,
+        projectData,
+    }
+});

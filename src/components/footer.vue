de22c04df84439ebea7d6dec288378ae7867ad9f
GayashanGamage <gayashan.randimagamage@gmail.com>
2026-02-18 12:57:33 +0530

    add footer section

---
+++ src/components/footer.vue
@@ -0,0 +1,6 @@
+<template>
+    <div class="grid gap-20 border w-[80%] p-10 mx-auto text-center">
+        <p class="">© 2025 Gayashan Gamage | Built with vuejs.</p>
+    </div>
+</template>
+<script setup></script>

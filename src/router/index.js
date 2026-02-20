bd219edb592567417bbac83cdeb46ff1ec7c6a96
GayashanGamage <gayashan.randimagamage@gmail.com>
2026-02-20 13:45:34 +0530

    correct the router page

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,6 @@
 
 import Index from "@/views/index.vue";
 import { createRouter, createWebHistory } from "vue-router";
-import test from "@/views/test.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
